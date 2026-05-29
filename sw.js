86f167476e635567e969cb06557eb18a183f10e2
sge444 <sge444@ya.ru>
2026-05-29 09:07:10 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'v1_cache';
+const ASSETS = [
+  'o_clock1.4.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+// Установка воркера и кэширование ресурсов
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Перехват запросов для работы офлайн
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
